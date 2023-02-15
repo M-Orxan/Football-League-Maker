dde59ee2889e3e8ff781d3a2a4af872d5d2884a3
M-Orxan <orxanm385@gmail.com>
2023-02-15 22:00:03 +0400

    AddingProjectToGithub

---
+++ Football League Maker/Football League Maker/Controllers/LeaguesController.cs
@@ -0,0 +1,23 @@
+﻿using Football_League_Maker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Football_League_Maker.Controllers
+{
+    public class LeaguesController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Football League Maker/Football League Maker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Football_League_Maker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
